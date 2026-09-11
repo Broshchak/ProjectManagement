f3cf408fd800e692f0367fd4008185ae11f8562e
Quantum345 <w2424184@gmail.com>
2026-09-12 02:37:40 +0300

    Add entity classes for order, order item, order status, order status entity, product category and product.

---
+++ Domain/Entities/OrderStatusHistoryEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class OrderStatusHistoryEntity
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int? PreviousStatusId { get; set; }
+        public int NewStatusId { get; set; }
+        public int? ChangedByUserId { get; set; }
+        public DateTime ChangedAt { get; set; }
+        public string? Comment { get; set; }
+    }
+}
